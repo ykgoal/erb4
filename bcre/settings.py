586e161aa0b29c2b21c4a66331f209b718d9c63f
ykgoal <ykgoal@gmail.com>
2024-12-20 08:57:20 +0800

    Step 9 postgres update

--- bcre/settings.py
+++ bcre/settings.py
@@ -77,10 +77,23 @@ WSGI_APPLICATION = 'bcre.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+'''
+
+DATABASES = {
+    'default': {
+        'ENGINE' : 'django.db.backends.postgresql',
+        'NAME' : 'bcredb',
+        'USER' : 'postgres',
+        'PASSWORD' : '123',
+        'HOST' : 'localhost',
     }
 }
 
--- bcre/urls.py
+++ bcre/urls.py
@@ -22,5 +22,4 @@ urlpatterns = [
     path('listings', include('listings.urls')), 
     path('admin/', admin.site.urls),
 
-
 ]
