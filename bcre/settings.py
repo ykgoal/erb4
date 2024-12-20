50d7e3c1552d4f24a2f2abc0a527e965d345f6e0
ykgoal <ykgoal@gmail.com>
2024-12-20 17:11:10 +0800

    Step 11 class update

--- bcre/settings.py
+++ bcre/settings.py
@@ -144,3 +144,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# media folder
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
